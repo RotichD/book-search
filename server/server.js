9f063aa0cf122165023d898eb5067d3b6522fe5d
4FRO <drotich@ucsc.edu>
2021-09-15 22:28:35 -0700

    reverted server file

--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-// const { ApolloServer } = require('apollo-server-express'); //Apollo Server
-
-// import typeDefs and resovlers
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
