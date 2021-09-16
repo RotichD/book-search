209240b6f561cdcca209a2bcbb2d3f93480264d3
4FRO <drotich@ucsc.edu>
2021-09-15 22:52:47 -0700

    trying to fix heroku deployment

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -29,8 +29,7 @@ module.exports = {
     }
 
     // send to next endpoint
-    //next();
-    return req;
+    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
