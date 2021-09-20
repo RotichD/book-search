b1208483192e54f19420212ee6af206f7343958f
4FRO <drotich@ucsc.edu>
2021-09-19 20:52:53 -0700

    * Fixed Server GraphQl * Added ApolloProvider * Fixed Resolver Schema Error

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
-const { typeDefs, resolver } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
 
 server.applyMiddleware({ app });
 
