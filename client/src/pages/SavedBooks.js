00a7e27dc38361b53041f893e64e02a71722a42b
4FRO <drotich@ucsc.edu>
2021-09-19 23:01:30 -0700

    changed front end connections

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 import { REMOVE_BOOK } from '../utils/mutations';
-import { QUERY_GET_ME } from '../utils/queries.js';
+import { QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const { loading, data: userData } = useQuery(QUERY_GET_ME);
-  console.log(userData);
+  const { loading, data: userData } = useQuery(QUERY_ME);
   const [deleteBook] = useMutation(REMOVE_BOOK);
 
   const token = Auth.loggedIn() ? Auth.getToken() : null;
